b2cd57311eb83e2bd4f91e5fed4666d449bcde4c
heokim <57774541+heokim@users.noreply.github.com>
2023-07-11 22:00:16 -0400

    init and api url changed

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { Product } from '../../models/product.model';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
+})
+export class ProductComponent {
+  @Input() product: Product = {
+    id: '',
+    price: 0,
+    images: [],
+    title: '',
+    category: {
+      id: '',
+      name: '',
+    },
+    description: '',
+  };
+  @Output() addedProduct = new EventEmitter<Product>();
+
+  constructor() {}
+
+  onAddToCart() {
+    this.addedProduct.emit(this.product);
+  }
+}
---
+++ src/app/models/product.model.ts
@@ -0,0 +1,13 @@
+export interface Product {
+  id: string;
+  title: string;
+  price: number;
+  images: string[];
+  description: string;
+  category: Category;
+}
+
+export interface Category {
+  id: string;
+  name: string;
+}
