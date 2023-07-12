01f8f22a157c4d8df71637abdf153131a46e66e3
heokim <57774541+heokim@users.noreply.github.com>
2023-07-12 14:12:49 -0400

    Reintenter Perticiones

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { retry } from 'rxjs/operators';
 
 import {
   CreateProductDTO,
@@ -20,13 +21,15 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', offset);
     }
-    return this.http.get<Product[]>(this.apiUrl, { params });
+    return this.http.get<Product[]>(this.apiUrl, { params }).pipe(retry(3));
   }
 
   getProdcductsByPage(limit: number, offset: number) {
-    return this.http.get<Product[]>(this.apiUrl, {
-      params: { limit, offset },
-    });
+    return this.http
+      .get<Product[]>(this.apiUrl, {
+        params: { limit, offset },
+      })
+      .pipe(retry(3));
   }
 
   getProduct(id: string) {
